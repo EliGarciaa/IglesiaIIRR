d232bb4021076eb67aa3409018f936ded172e3be
root <root@localhost.localdomain>
2016-10-04 09:27:28 -0600

    Amen Gloria a DIOS

---
+++ js/ControladorAjax.js
@@ -0,0 +1,15 @@
+function entrar(){
+	var dato1 = document.getElementById("usuario").value;
+	var dato2 = document.getElementById("contraseña").value;
+
+	if (dato1 == "Iglesia" && dato2 == "Rey123") {
+		location.href = "menu.html";
+	} else if (dato1 == "" && dato2 == "") {
+		alert("Ingresar Datos");
+	} else {
+		alert("Error, verificar datos");
+	}
+
+	document.getElementById("usuario").value = "";
+	document.getElementById("contraseña").value = "";
+}
